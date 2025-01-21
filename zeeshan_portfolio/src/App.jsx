2f032d14fcf035ca7d48778b40104b99ae9a4437
tayyabmushtaq15 <tayyabmushtaq15@gmail.com>
2025-01-21 16:59:43 +0500

    First commit by Tayyab React Dev

---
+++ zeeshan_portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Experience from './components/Experience'
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Experience />
+    </>
+  )
+}
+
+export default App
---
+++ zeeshan_portfolio/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='container navBar'>
+        <div className='left nav-Item'>
+        Welcome to my Profile
+        </div>
+        <div className='right'>
+            <a href="" className='nav-Item'>Home</a>
+            <a href="" className='nav-Item'>Experience</a>
+            <a href="" className='nav-Item'>Skills</a>
+            <a href="" className='nav-Item'>Project</a>
+            <a href="" className='nav-Item'>Contact</a>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
