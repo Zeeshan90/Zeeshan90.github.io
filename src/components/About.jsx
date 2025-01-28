a38e0251ed3eea47e5d0d2f47e174653abf3483f
tayyabmushtaq15 <tayyabmushtaq15@gmail.com>
2025-01-28 14:11:21 +0500

    Initial commit for Heroku

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,23 +2,24 @@ import React from 'react'
 import zeeImg from '../assets/zeeshanImg.jpeg'
 import resumPdf from '../assets/resume/resume.pdf'
 
-const Home = () => {
+const About = () => {
   return (
     <>
     <div className='container home'>
-      <div className='left'>
-      <h1>M Zeeshan Ashraf</h1>
-      <p>Senior Mobile Application Developer</p>
-      <a href={resumPdf} download="M Zeeshan Ashraf.pdf" className="btn btn-outline-warning">Download Resume</a>
-      </div>
+      
       <div className='right'>
         <div className='zeeImg'>
           <img src={zeeImg} alt="Zeeshan Ashraf" />
         </div>
+      </div>
+      <div className='left'>
+      <h1>M Zeeshan Ashraf</h1>
+      <p>Senior Mobile Application Developer</p>
+      <a href={resumPdf} download="M Zeeshan Ashraf.pdf" className="btn btn-outline-primary">Download Resume</a>
       </div>
     </div>
     </>
   )
 }
 
-export default Home
+export default About
